4fb2ee333e085790eb380335c1b07a7cbb50d635
Fromundus <ninjadentist24@gmail.com>
2026-09-23 11:12:48 +0800

    allowed public routes for user side

--- routes/api.php
+++ routes/api.php
@@ -55,14 +55,16 @@ Route::middleware(['auth:sanctum'])->group(function(){
 });
 
 Route::post('/login', [AuthController::class, 'login']);
-// Route::get('/tickets/count', [TicketController::class, 'count']);
-// Route::post('/tickets', [TicketController::class, 'store']);
+Route::get('/tickets/count', [TicketController::class, 'count']);
+Route::post('/tickets', [TicketController::class, 'store']);
 
-// Route::prefix('/devices')->group(function(){
-//     Route::get('/', [DeviceController::class, 'index']);
-// });
+Route::prefix('/devices')->group(function(){
+    Route::get('/', [DeviceController::class, 'index']);
+});
 
-// Route::get('/tickets/{uid}', [TicketController::class, 'show']);
+Route::get('/tickets/{uid}', [TicketController::class, 'show']);
+
+Route::get('/employees-all', [EmployeeController::class, 'all']);
 
 Route::get('/test', function(){
     return response()->json([
